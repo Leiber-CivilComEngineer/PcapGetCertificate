f6b4db78d18c862bccecc069369860b169c35051
Leiber Baoqian Lyu <leiber1117@gmail.com>
2023-07-21 16:59:29 +1000

    add ignore, update readme, rename cpp file

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # PcapGetCertificate
-Analize Pcap file and extract SSL certificates
+
+This is a C++ program designed to extract SSL certificates from pcap files. The program utilizes Wireshark for pcap packet parsing, extracts the byte stream of certificates, and then saves them as standard DER format files.
+
+
+
+## Features
+
+* This is a C++ program developed using the Wireshark API called "tshark" to parse pcap files without a graphical user interface (GUI).
+* The program is simple yet capable of handling large pcap files efficiently. It utilizes the power of the tshark API to process and parse even large-sized pcap files without compromising performance. This enables it to handle network captures with substantial amounts of data, making it suitable for analyzing and extracting SSL certificates from extensive network traffic recordings. Its ability to manage large pcap files ensures that it can handle diverse real-world network scenarios and provide valuable insights into SSL certificate usage within complex network environments.
+
+
+
+## Getting Started
+
+### Test Environment
+
+This is the Env that is used to develop this program. Different versions of the development environment may not be able to run the program.
+
+* **TShark (Wireshark) 3.2.3 ** - found at [https://www.wireshark.org/](https://cmake.org/)
+* **gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1)** 
+
+
+
+## Build and Run the project
+
+To build the project, all you need to do, ***after correctly
+[installing the project](README.md#Installing)***, is run a g++ command:
+
+```bash
+g++ get_certificates.cpp -o get_certificates
+./get_certificates SSL.pcappng
+```
+
+> ***Note:*** *SSL.pcappng is the pcap file that you want to work on*.
+
+## Resulting Certificates
+
+After the program finishes running, all the certificates obtained will be saved in DER format within the "./certificates" folder.
+
+<img src="/Users/leiber/Library/Application Support/typora-user-images/image-20230721164020803.png" alt="image-20230721164020803" style="zoom: 67%;" />
+
+## Contributing
+
+Not available now
+
+## Versioning
+
+Not available now
+
+## Authors
+
+* **Leiber Baoqian LYU** - [@Leiber](https://github.com/Leiber-CivilComEngineer)
+
+## License
+
+Not available now
+
+## References
+
+- https://www.wireshark.org/docs/dfref/t/tls.html
+- https://www.wireshark.org/docs/wsdg_html/
+- https://www.wireshark.org/docs/
+- https://richardatkin.com/post/2022/01/15/Identifying-and-retrieving-certificates-from-a-PCAP-file-using-Wireshark.html
+- https://blog.csdn.net/u014786330/article/details/88399498
+- https://www.cnblogs.com/bonelee/p/13522166.html
+- https://osqa-ask.wireshark.org/questions/41034/extract-certificate-info-with-tshark/
+- https://serverfault.com/questions/313610/extracting-ssl-certificates-from-the-network-or-pcap-files
+- https://21xrx.com/Articles/read_article/175130
+- https://www.youtube.com/watch?v=-HDpYR_QSFw&list=PLW8bTPfXNGdC5Co0VnBK1yVzAwSSphzpJ&index=5
+- https://www.youtube.com/watch?v=IlVppluWTHw&list=PLW8bTPfXNGdC5Co0VnBK1yVzAwSSphzpJ&index=10&pp=iAQB&themeRefresh=1
+- https://www.geeksforgeeks.org/tcp-ip-model/
+- https://chat.openai.com/
--- get_certificates.cpp
+++ get_certificates.cpp
@@ -3,16 +3,21 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <sys/stat.h> // For mkdir function
+#include <sys/stat.h>
 
 // Function to convert a hexadecimal string to binary data
+// This function takes a string of hexadecimal characters and converts it into a sequence of binary data.
+// The binary data is stored in a vector of unsigned characters and returned.
 std::vector<unsigned char> hexToBinary(const std::string& hex) {
     std::vector<unsigned char> binary;
     binary.reserve(hex.length() / 2);
 
+    // Iterate through the hexadecimal string two characters at a time
     for (std::size_t i = 0; i < hex.length(); i += 2) {
         unsigned int byte;
+        // Extract two characters from the string and convert them to an unsigned integer in hexadecimal format
         std::istringstream(hex.substr(i, 2)) >> std::hex >> byte;
+        // Convert the unsigned integer to an unsigned character and store it in the binary vector
         binary.push_back(static_cast<unsigned char>(byte));
     }
 
@@ -20,16 +25,22 @@ std::vector<unsigned char> hexToBinary(const std::string& hex) {
 }
 
 int main(int argc, char* argv[]) {
+    // Check if the program is provided with the correct number of command-line arguments
+    // The program should be executed with the name of the input file as an argument.
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file.pcapng>" << std::endl;
         return 1;
     }
 
+    // Get the input file name from the command-line argument
     std::string input_file = argv[1];
 
     // Create 'certificates' directory if it does not exist
+    // The program creates a directory named 'certificates' to store the output files if it doesn't already exist.
     const char* output_dir = "certificates";
     if (mkdir(output_dir, 0755) == -1) {
+        // If the directory creation fails and the error is not that the directory already exists,
+        // display an error message and terminate the program.
         if (errno != EEXIST) {
             std::cerr << "Error creating 'certificates' directory." << std::endl;
             return 1;
@@ -37,26 +48,36 @@ int main(int argc, char* argv[]) {
     }
 
     // Execute tshark command and store the output in certificates.txt
+    // The program runs the "tshark" command-line utility to analyze the input file for SSL handshake certificates.
+    // The "-r" option specifies the input file, "-Y" filters for SSL handshake certificates,
+    // "-T fields" specifies the output format as a field list, and "-e tls.handshake.certificate" specifies the certificate field.
+    // The output is redirected to a file named "certificates.txt".
     std::string tshark_command = "tshark -r " + input_file + " -Y \"ssl.handshake.certificate\" -T fields -e tls.handshake.certificate > certificates.txt";
     system(tshark_command.c_str());
 
     // Read the certificates.txt file
+    // The program opens the "certificates.txt" file to read the SSL handshake certificates extracted by tshark.
     std::ifstream infile("certificates.txt");
     std::string line;
     int counter = 1;
 
     // Process each line from the txt file
+    // The program processes each line of the "certificates.txt" file, which contains hexadecimal strings of the certificates.
     while (std::getline(infile, line)) {
         // Convert the current line to binary DER format
+        // The program uses the "hexToBinary" function to convert the hexadecimal string read from the file into binary data in DER format.
         std::vector<unsigned char> certificate_data = hexToBinary(line);
 
         // Write the certificate data to a binary DER file in 'certificates' directory
+        // The program creates binary DER files for each certificate in the "certificates" directory.
+        // The file names are in the format "certificate_<counter>.der" where <counter> is a number starting from 1.
         std::string filename = output_dir + std::string("/certificate_") + std::to_string(counter) + ".der";
         std::ofstream outfile(filename, std::ios::binary);
         outfile.write(reinterpret_cast<char*>(&certificate_data[0]), certificate_data.size());
         outfile.close();
 
         // Increment the counter for the next certificate
+        // The program increments the counter to prepare for processing the next certificate.
         counter++;
     }
 
